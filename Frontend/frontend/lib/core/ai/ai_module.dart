ec69eda2b54bcd4e86e3a67373090c6a42871e3a
phuctran33 <tranthienphuc3306@gmail.com>
2025-12-20 14:40:21 +0700

    Move AI modules

--- Frontend/frontend/lib/core/ai/ai_module.dart
+++ Frontend/frontend/lib/core/ai/ai_module.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'models/ai_models.dart';
+import 'ai_models.dart';
 
 class AIModule {
   /// URL Backend - Có thể configure khi khởi tạo app
